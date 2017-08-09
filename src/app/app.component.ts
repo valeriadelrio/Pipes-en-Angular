1de5011a307e1ce5a271c4211b554fccfdf97e98
valeriadelrio <valeriadlrio@gmail.com>
2017-08-09 17:08:33 -0300

    Utilización del pipe JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   nombre = "Valeria";
+
   arreglo = [1,2,3,4,5,6,7,8,9,10];
+
   PI= Math.PI;
+
   a = 0.234;
+
   salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      numero: 19
+    }
+  }
+
 }
